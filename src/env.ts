340ccbc89ecaf05682f81ad4831ca27dc8413e22
Jesse Quartey <58194213+jessequartey@users.noreply.github.com>
2025-06-16 05:58:28 +0000

    add skipValidation option to environment configuration for improved flexibility

--- src/env.ts
+++ src/env.ts
@@ -12,6 +12,8 @@ export const env = createEnv({
     NOTION_API_KEY: process.env.NOTION_API_KEY,
     NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
   },
+
+  skipValidation: true,
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   // experimental__runtimeEnv: {
   //   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
